bd9e771e3e8d20f6428bf31cd3d7ac7dec6c0414
MajdHamdallah <majdhamdallah42@gmail.com>
2023-09-07 18:03:20 -0700

    Java Homework

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Passowrd Generator
+
+## Description
+
+I have developed a password generator as a demonstration of my proficiency in JavaScript. This web application offers a user-friendly interface where, upon clicking the "Generate Password" button, a sequence of prompts is presented to the user. These prompts empower the user to specify the desired password length, ranging to select character types, including enlgish letters, numbers, and  character symbols.![Alt text](<Screen Shot 2023-09-07 at 5.52.04 PM.png>)
+
+## Installation
+
+
+
+## Usage
+The primary purpose of this password generator is to facilitate the creation of secure and customizable passwords for various online accounts and services. It empowers users to generate strong and unique passwords by specifying their desired length and character composition, enhancing online security and reducing the risk of unauthorized access to their accounts. 
+
+## Credits
+
+Majd Hamdallah
+jacob Nordan (Tutor)
---
+++ script.js
@@ -0,0 +1,69 @@
+// Assignment Code
+
+// Create some variable, that hold each character value e.g. (var this = "ABC...".split(""), var this = "123...") => ["A", "B"]
+
+// using prompt, how can we get a length, and store it for later
+
+// maybe build out a GIANT array based on what the user wants e.g. ["A", "B"... , "a", "b"]  using maybe .push() .concat() 
+// when asking for what they want in their password
+
+// for loop through the giant array, and using var random = Math.floor(Math.random() * giantArray.length) you can grab random values
+
+// password += giantArray[random]
+
+// Write password to the #password input
+document.getElementById("generate").addEventListener("click", function() {
+  let length = prompt("Enter password length between 8 and 128 characters:");
+
+  if (length < 8 || length > 128) {
+      alert("Invalid password length. Please try again.");
+      return;
+  }
+
+  let useLower = confirm("Do you want to include lowercase letters?");
+  let useUpper = confirm("Do you want to include uppercase letters?");
+  let useNumbers = confirm("Do you want to include numbers?");
+  let useSpecial = confirm("Do you want to include special characters?");
+
+  if (!(useLower || useUpper || useNumbers || useSpecial)) {
+      alert("Type at least one character. You can try again.");
+      return;
+  }
+
+  let charset = "";
+  let password = "";
+
+  if (useLower) charset += "abcdefghijklmnopqrstuvwxyz";
+  if (useUpper) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (useNumbers) charset += "0123456789";
+  if (useSpecial) charset += "!@#$%^&*()-_=+";
+
+  for (let i = 0; i < length; i++) {
+      let randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+  }
+
+  document.getElementById("password").innerText = password;
+
+  // Enable the "Copy to Clipboard" button only after generating the password
+  document.getElementById("copy").removeAttribute("disabled");
+  document.getElementById("copy").style.cursor = "pointer";
+});
+
+document.getElementById("copy").addEventListener("click", function() {
+  let passwordText = document.getElementById("password").innerText;
+  
+  if (passwordText === "") {
+      // If there's no password, disable the "Copy to Clipboard" button
+      document.getElementById("copy").setAttribute("disabled", "disabled");
+      document.getElementById("copy").style.cursor = "not-allowed";
+      alert("No password to copy");
+      return;
+  }
+  
+  navigator.clipboard.writeText(passwordText).then(function() {
+      alert("Password copied to clipboard!");
+  }).catch(function() {
+      alert("Failed to copy the password to clipboard.");
+  });
+});
